0486f65c0dfc7d443c691d2f33900b2aa7310c38
Doç. Dr. Efe Edem <efee96882@gmail.com>
2025-11-18 16:54:52 +0400

    i

--- mardinlee/api/activity.js
+++ mardinlee/api/activity.js
@@ -2,6 +2,17 @@
 const { connectToDatabase } = require('./lib/mongodb');
 
 module.exports = async (req, res) => {
+    // CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Content-Type', 'application/json');
+
+    // Handle OPTIONS request
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     try {
         const { db } = await connectToDatabase();
 
